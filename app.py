1b08515f395def5d14ab7041a52cd49ebf9d6f6f
Yuki <k701user@gmail.com>
2018-06-04 09:23:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -90,11 +90,11 @@ def processRequest(req):
         pass
     print(actiontype)
     if actiontype == "reply_to_player_record":
-        res = SL.execute_sql(date, name, "bplayerrecord", "name", ["name", "record"])
+        res = SL.execute_sql(name, "bplayerrecord", "name", ["name", "record"], day=date)
     elif actiontype == "reply_to_news":
-        res = SL.execute_sql(date, name, "newsrecord", "name", ["title", "row2_text"])
+        res = SL.execute_sql(name, "newsrecord", "name", ["title", "row2_text"], day=date)
     elif actiontype == "reply_to_soccer_score" or actiontype == "reply_to_baseball_score":
-        res = SL.execute_sql2(date, [team1, team2],"scorerecord", ["team1", "team2"], ["team1", "team2", "score"])
+        res = SL.execute_sql2([team1, team2],"scorerecord", ["team1", "team2"], ["team1", "team2", "score"], day=date)
     else:
         return {}
 
